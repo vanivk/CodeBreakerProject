cef86b7f8663484adf56adb0cf2ece0cfdb289d2
Vani Kulkarni <vani.kulkarni@mesitis.com>
2018-04-06 16:19:34 +0530

    Commit: Missed adding "length"

--- src/assets/main.js
+++ src/assets/main.js
@@ -51,7 +51,7 @@ function getResults(input) {
 }
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 10000).toString();
-    while (answer.value < 4) {
+    while (answer.value.length < 4) {
         answer.value = "0" + answer.value;
     }
     attempt.value = "0";
